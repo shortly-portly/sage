dbbeddb3cfe20a442d79eda5bb407fdbd5e52685
David Simmons <shortlyportly@googlemail.com>
2021-03-07 08:58:43 +0000

    Get migration working on Gigalixir

--- priv/repo/migrations/20210228081830_create_companies.exs
+++ priv/repo/migrations/20210228081830_create_companies.exs
@@ -1,7 +1,7 @@
 defmodule Sage.Repo.Migrations.CreateCompanies do
   use Ecto.Migration
 
-  def change do
+  def up do
     create table(:companies) do
       add :name, :string
       add :address_line_1, :string
@@ -27,5 +27,9 @@ defmodule Sage.Repo.Migrations.CreateCompanies do
 
     # create unique_index(:companies, [:name, :organisation_id])
 
+  end
+
+  def down do
+    drop table :companies
   end
 end
